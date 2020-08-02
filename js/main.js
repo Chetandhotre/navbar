6f140c4a371d2444f2fed949e790cad465b838ed
root <root@localhost>
2020-08-02 13:00:09 +0530

    publishing awesome pages 1596353409432

---
+++ js/main.js
@@ -0,0 +1,50 @@
+// Add all your JS here
+(function() {
+ const header = document.querySelector('.header');
+ const icon = document.querySelector('.icon-container');
+ icon.onclick = function() {
+  header.classList.toggle('menu-open');
+ }
+}());
+
+
+/* search */
+function onPage() { 
+
+ var a = document.getElementById("search").value; 
+ 
+ if (a === "chetan") {
+  window.open("https://dcgroup.ml/developer.html");
+   }
+ if (a === "mr majnu") {
+  window.open("https://dcgroup.ml/one.html");
+   }
+ if (a === "a gentleman") {
+  window.open("https://dcgroup.ml/two.html");
+   }
+ if (a === "avenger endgame") {
+  window.open("https://dcgroup.ml/three.html");
+   }
+ if (a === "captain marvel") {
+  window.open("https://dcgroup.ml/four.html");
+   }
+ if (a === "jay love kush" ) {
+  window.open("https://dcgroup.ml/five.html");
+   }
+ if (a ==="dr strange" ) {
+  window.open("https://dcgroup.ml/six.html");
+   }
+ if (a ==="infinity war") {
+  window.open("https://dcgroup.ml/seven.html");
+   }
+ if (a === "kgf" ) {
+  window.open("https://dcgroup.ml/eight.html");
+   }
+ if (a === "bollywood") {
+  window.open("https://dcgroup.ml/bollywood.html");
+   }
+ if (a === "south hindi" ) {
+  window.open("https://dcgroup.ml/south%20hindi.html");
+   }
+}
+
